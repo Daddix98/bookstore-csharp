5db7a625548dde0f2cddcb84b951156da453598b
Davide Scriboni <davidescriboni94@gmail.com>
2022-04-04 16:24:15 +0200

    Prima Parte esercizio

--- BookStore Csharp/Program.cs
+++ BookStore Csharp/Program.cs
@@ -1,2 +1,40 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+// Esercizio Book-Store-c#
+
+Console.WriteLine("Informazioni inerenti al libbro CleanCode:");
+
+//Elenco informazioni:
+
+string titolo = "Clean Code";
+string nomeAutore = "Robert C. Martin";
+long ISBN = 9780132350884;
+int numeroPagine = 431;
+int grammiLibro = 660;
+double lunghezzaLibro = 17.91;
+double altezzaLibro = 21.5;
+double profonditàLibro = 2.54;
+float valutazioneMedia = 4.7F;
+int recenzioniLibro = 3309;
+string versioneKidle = "SI";
+string copertiraFlessibile = "si";
+
+Console.WriteLine("Titolo : " + titolo + "\nNome Autore : " +  nomeAutore + "\nISBN : " + ISBN + "\nNumero Pagine : "
+    + numeroPagine + "\nPeso Libro : " +grammiLibro + "\nLunghezza Libro : " + lunghezzaLibro+
+"\nAltezza Libro : " + altezzaLibro + "\nProfondità Libro : " + profonditàLibro + "\nValutazione Media : " + valutazioneMedia +
+"\nRecensioni Libro : " + recenzioniLibro + "\nVersione Kidle : " + versioneKidle + "\nCopertina Flessibile : " + copertiraFlessibile);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
